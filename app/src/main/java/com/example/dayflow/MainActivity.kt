75f66cde44ff47fba0412858c79ea49fe5ec3a05
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-12-04 17:21:34 +0200

    add AppDestination

--- app/src/main/java/com/example/dayflow/MainActivity.kt
+++ app/src/main/java/com/example/dayflow/MainActivity.kt
@@ -4,6 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.dayflow.navigation.AppDestination
 import com.example.dayflow.ui.theme.DayFlowTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,7 +23,20 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DayFlowTheme {
-
+                Scaffold { innerPadding ->
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(MaterialTheme.colorScheme.background)
+                            .padding(innerPadding)
+                    ) {
+                        val navController = rememberNavController()
+                        NavHost(navController, startDestination = AppDestination.DailyTasks) {
+                            composable<AppDestination.DailyTasks> { }
+                            composable<AppDestination.YearlyGoals> { }
+                        }
+                    }
+                }
             }
         }
     }
