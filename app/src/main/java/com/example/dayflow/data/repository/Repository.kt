07becc5cb3d40a97fa030fcbe1e3ff01e7e47c48
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-12-07 18:34:45 +0200

    implement require tasks operation in repo

--- app/src/main/java/com/example/dayflow/data/repository/Repository.kt
+++ app/src/main/java/com/example/dayflow/data/repository/Repository.kt
@@ -1,4 +1,10 @@
 package com.example.dayflow.data.repository
 
+import com.example.dayflow.data.local.entity.TaskEntity
+
 interface Repository {
+
+    suspend fun addTask(task: TaskEntity)
+    suspend fun getAllTasks(): List<TaskEntity>
+    suspend fun deleteTask(id: Int)
 }
--- app/src/main/java/com/example/dayflow/data/repository/RepositoryImpl.kt
+++ app/src/main/java/com/example/dayflow/data/repository/RepositoryImpl.kt
@@ -1,9 +1,22 @@
 package com.example.dayflow.data.repository
 
 import com.example.dayflow.data.local.LocalDataBase
+import com.example.dayflow.data.local.entity.TaskEntity
 import javax.inject.Inject
 
 class RepositoryImpl @Inject constructor(
     private val localDataBase: LocalDataBase,
 ) : Repository {
+
+    override suspend fun addTask(task: TaskEntity) {
+        localDataBase.taskDao().addTask(task)
+    }
+
+    override suspend fun getAllTasks(): List<TaskEntity> {
+        return localDataBase.taskDao().getAllTasks()
+    }
+
+    override suspend fun deleteTask(id: Int) {
+        localDataBase.taskDao().deleteTask(id)
+    }
 }
