c965a26ca11293dedeb3633ffc0502684e19649e
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-12-08 20:02:26 +0200

    create primary text field & button

---
+++ app/src/main/java/com/example/dayflow/ui/composable/PrimaryTextButton.kt
@@ -0,0 +1,45 @@
+package com.example.dayflow.ui.composable
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.example.dayflow.ui.theme.spacing
+
+
+@Composable
+fun PrimaryTextButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    containerColor: Color = MaterialTheme.colorScheme.primary,
+    contentColor: Color = MaterialTheme.colorScheme.onPrimary,
+    border: BorderStroke? = null,
+    isEnabled: Boolean = true,
+    onClick: () -> Unit,
+) {
+    TextButton(
+        modifier = modifier,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = containerColor,
+            contentColor = contentColor,
+            disabledContainerColor = MaterialTheme.colorScheme.outline,
+            disabledContentColor = MaterialTheme.colorScheme.outlineVariant
+        ),
+        shape = RoundedCornerShape(MaterialTheme.spacing.space4),
+        contentPadding = PaddingValues(MaterialTheme.spacing.space16),
+        border = border,
+        enabled = isEnabled,
+        onClick = onClick
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.titleMedium,
+        )
+    }
+}
