57e9ef579d87d2903060e5762e628df84d835c6b
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-12-08 13:53:41 +0200

    add TaskUiState

---
+++ app/src/main/java/com/example/dayflow/ui/utils/ui_state/TaskUiState.kt
@@ -0,0 +1,9 @@
+package com.example.dayflow.ui.utils.ui_state
+
+data class TaskUiState(
+    val id: Int = 0,
+    val title: String = "",
+    val description: String = "",
+    val date: String = "",
+    val isDone: Boolean = false,
+)
