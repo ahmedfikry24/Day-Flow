45cb98425a3b244b83ea8503e2fe1757db48b0a6
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-12-15 20:31:13 +0200

    update manifest

--- app/src/main/java/com/example/dayflow/data/utils/DataConstants.kt
+++ app/src/main/java/com/example/dayflow/data/utils/DataConstants.kt
@@ -4,5 +4,8 @@ object DataConstants {
     const val TASK_ID = "taskId"
     const val TASK_TITLE = "taskTitle"
     const val WORKER_NAME = "daily_task_worker"
-
+    const val TRIGGER_NOTIFICATION_ACTION = "triggerNotificationAction"
+    const val NOTIFICATION_CHANNEL_ID = "channelId"
+    const val NOTIFICATION_CHANNEL_NAME = "channelName"
+    const val ALARM_STOP_ACTION = "ALARM STOP ACTION"
 }
