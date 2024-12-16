6de2a6bfa62a93eff5944df68fac28ce4319c881
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-12-16 20:44:45 +0200

    remove work manager implementation

--- app/src/main/java/com/example/dayflow/di/DayFlowApplication.kt
+++ app/src/main/java/com/example/dayflow/di/DayFlowApplication.kt
@@ -1,26 +1,7 @@
 package com.example.dayflow.di
 
 import android.app.Application
-import androidx.hilt.work.HiltWorkerFactory
-import androidx.work.Configuration
-import com.example.dayflow.data.worker.scheduleDailyWorkManagerRequest
 import dagger.hilt.android.HiltAndroidApp
-import javax.inject.Inject
 
 @HiltAndroidApp
-class DayFlowApplication : Application(), Configuration.Provider {
-
-    @Inject
-    lateinit var workerFactory: HiltWorkerFactory
-
-    override fun onCreate() {
-        super.onCreate()
-        scheduleDailyWorkManagerRequest(this)
-    }
-
-
-    override val workManagerConfiguration: Configuration
-        get() = Configuration.Builder()
-            .setWorkerFactory(workerFactory)
-            .build()
-}
+class DayFlowApplication : Application()
