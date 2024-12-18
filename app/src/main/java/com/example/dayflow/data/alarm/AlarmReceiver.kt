a2abc1fe167e903e739d54bc30d11f85eaaa2a13
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2024-12-18 13:58:46 +0200

    isolate notification & alarm services

--- app/src/main/java/com/example/dayflow/data/alarm/AlarmReceiver.kt
+++ app/src/main/java/com/example/dayflow/data/alarm/AlarmReceiver.kt
@@ -1,19 +1,12 @@
 package com.example.dayflow.data.alarm
 
-import android.Manifest
-import android.app.NotificationChannel
-import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.media.RingtoneManager
-import android.util.Log
-import androidx.core.app.NotificationCompat
-import androidx.core.content.ContextCompat
-import com.example.dayflow.R
 import com.example.dayflow.data.utils.DataConstants
+import com.example.dayflow.data.utils.DefaultNotificationManager
+import com.example.dayflow.data.utils.NotificationArgs
 
 class AlarmReceiver : BroadcastReceiver() {
 
@@ -40,49 +33,29 @@ class AlarmReceiver : BroadcastReceiver() {
     }
 
     private fun showAlarmNotification(context: Context, title: String, taskId: Int) {
-        if (
-            ContextCompat.checkSelfPermission(
-                context,
-                Manifest.permission.POST_NOTIFICATIONS
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-
-            val notificationManager =
-                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            val channel = NotificationChannel(
-                DataConstants.NOTIFICATION_CHANNEL_ID,
-                DataConstants.NOTIFICATION_CHANNEL_NAME,
-                NotificationManager.IMPORTANCE_HIGH
-            )
-            channel.enableVibration(true)
-            notificationManager.createNotificationChannel(channel)
-
-            val stopIntent = Intent(context, AlarmReceiver::class.java).apply {
-                action = DataConstants.ALARM_STOP_ACTION
-                putExtra(DataConstants.TASK_ID, taskId)
-            }
-            val stopPendingIntent = PendingIntent.getBroadcast(
-                context,
-                taskId,
-                stopIntent,
-                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-            )
-            var alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
-            if (alarmUri == null)
-                alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-
-            val notificationBuilder =
-                NotificationCompat.Builder(context, DataConstants.NOTIFICATION_CHANNEL_ID)
-                    .setSmallIcon(R.drawable.logo)
-                    .setContentTitle(title)
-                    .setSound(alarmUri)
-                    .setAutoCancel(true)
-                    .addAction(R.drawable.ic_check_false, "Stop", stopPendingIntent)
-                    .setPriority(NotificationCompat.PRIORITY_HIGH)
-                    .setDefaults(NotificationCompat.DEFAULT_ALL)
-
-            notificationManager.notify(taskId, notificationBuilder.build())
+        val stopIntent = Intent(context, AlarmReceiver::class.java).apply {
+            action = DataConstants.ALARM_STOP_ACTION
+            putExtra(DataConstants.TASK_ID, taskId)
         }
+        val stopPendingIntent = PendingIntent.getBroadcast(
+            context,
+            taskId,
+            stopIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+        DefaultNotificationManager.showNotification(
+            context,
+            notificationArgs = NotificationArgs(
+                id = taskId,
+                title = title,
+                actionPendingIntent = stopPendingIntent
+            )
+        )
+    }
+
+    private fun cancelScheduledAlarm(context: Context, id: Int) {
+        DefaultAlarmManager.cancelAlarm(context, id)
+        DefaultNotificationManager.cancelNotification(context, id)
     }
 }
 
--- app/src/main/java/com/example/dayflow/data/alarm/scheduleTaskAlarm.kt
+++ app/src/main/java/com/example/dayflow/data/alarm/scheduleTaskAlarm.kt
@@ -1,12 +1,7 @@
 package com.example.dayflow.data.alarm
 
-import android.app.AlarmManager
-import android.app.PendingIntent
 import android.content.Context
-import android.content.Intent
-import android.os.Build
 import com.example.dayflow.data.local.entity.TaskEntity
-import com.example.dayflow.data.utils.DataConstants
 import com.example.dayflow.ui.utils.convertLongToDate
 import com.example.dayflow.ui.utils.convertLongToTime
 import java.text.SimpleDateFormat
@@ -15,47 +10,10 @@ import java.util.Locale
 import java.util.TimeZone
 
 fun scheduleTaskAlarm(context: Context, task: TaskEntity) {
-
     if (task.date == null || task.time == null) return
 
-    val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-
-    val intent = Intent(context, AlarmReceiver::class.java).apply {
-        action = DataConstants.TRIGGER_NOTIFICATION_ACTION
-        putExtra(DataConstants.TASK_ID, task.id)
-        putExtra(DataConstants.TASK_TITLE, task.title)
-    }
-
     val alarmTime = getAlarmTime(task.date.convertLongToDate(), task.time.convertLongToTime())
-
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-        if (alarmManager.canScheduleExactAlarms()) {
-            val pendingIntent = PendingIntent.getBroadcast(
-                context,
-                task.id,
-                intent,
-                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE
-            )
-            alarmManager.setExactAndAllowWhileIdle(
-                AlarmManager.RTC_WAKEUP,
-                alarmTime,
-                pendingIntent
-            )
-        }
-
-    } else {
-        val pendingIntent = PendingIntent.getBroadcast(
-            context,
-            task.id,
-            intent,
-            PendingIntent.FLAG_UPDATE_CURRENT
-        )
-        alarmManager.setExactAndAllowWhileIdle(
-            AlarmManager.RTC_WAKEUP,
-            alarmTime,
-            pendingIntent
-        )
-    }
+    DefaultAlarmManager.setAlarm(context, task.id, task.title, alarmTime)
 }
 
 private fun getAlarmTime(dateString: String, timeString: String): Long {
