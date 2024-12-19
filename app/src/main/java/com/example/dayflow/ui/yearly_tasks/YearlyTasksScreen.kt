ea1bbdfbf6b7232fe363aa57c4b35142eb637432
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-12-19 10:04:41 +0200

    design AddYearlyTask & refactor screen with new composable

--- app/src/main/java/com/example/dayflow/ui/yearly_tasks/YearlyTasksScreen.kt
+++ app/src/main/java/com/example/dayflow/ui/yearly_tasks/YearlyTasksScreen.kt
@@ -1,13 +1,16 @@
 package com.example.dayflow.ui.yearly_tasks
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -18,6 +21,7 @@ import com.example.dayflow.ui.composable.VisibleContent
 import com.example.dayflow.ui.theme.spacing
 import com.example.dayflow.ui.utils.ContentStatus
 import com.example.dayflow.ui.utils.EventHandler
+import com.example.dayflow.ui.yearly_tasks.composable.AddYearlyTask
 import com.example.dayflow.ui.yearly_tasks.composable.SwipeYearlyTask
 import com.example.dayflow.ui.yearly_tasks.vm.YearlyTasksInteractions
 import com.example.dayflow.ui.yearly_tasks.vm.YearlyTasksUiState
@@ -42,18 +46,26 @@ private fun YearlyTasksContent(
 ) {
     LoadingContent(isVisible = state.contentStatus == ContentStatus.LOADING)
     VisibleContent(isVisible = state.contentStatus == ContentStatus.VISIBLE) {
-        LazyColumn(
-            modifier = Modifier.fillMaxSize(),
-            contentPadding = PaddingValues(MaterialTheme.spacing.space16),
-            verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.space16)
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = MaterialTheme.spacing.space16),
+            contentAlignment = Alignment.BottomEnd
         ) {
-            items(state.tasks) { task ->
-                SwipeYearlyTask(
-                    state = task,
-                    onSwipeDelete = {},
-                    onClickTask = {}
-                )
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                contentPadding = PaddingValues(vertical = MaterialTheme.spacing.space16),
+                verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.space16)
+            ) {
+                items(state.tasks) { task ->
+                    SwipeYearlyTask(
+                        state = task,
+                        onSwipeDelete = {},
+                        onClickTask = {}
+                    )
+                }
             }
+            AddYearlyTask(state = state, interaction = interactions)
         }
     }
     ErrorContent(
