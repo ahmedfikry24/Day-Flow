498483cecd39865c16fd9924fb99cf763d0e8801
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-12-19 09:11:32 +0200

    create YearlyTaskDao

--- app/src/main/java/com/example/dayflow/data/local/LocalDataBase.kt
+++ app/src/main/java/com/example/dayflow/data/local/LocalDataBase.kt
@@ -3,10 +3,13 @@ package com.example.dayflow.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.dayflow.data.local.dao.DailyTaskDao
+import com.example.dayflow.data.local.dao.YearlyTaskDao
 import com.example.dayflow.data.local.entity.DailyTaskEntity
 
 @Database(entities = [DailyTaskEntity::class], version = 1)
 abstract class LocalDataBase : RoomDatabase() {
 
     abstract fun dailyTaskDao(): DailyTaskDao
+
+    abstract fun yearlyTaskDao(): YearlyTaskDao
 }
