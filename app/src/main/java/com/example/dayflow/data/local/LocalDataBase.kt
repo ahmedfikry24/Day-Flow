8152697df3bb8c823b2def3fce92dfbd44696181
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-12-19 10:18:13 +0200

    fix room db can't read yearlyTaskEntity

--- app/src/main/java/com/example/dayflow/data/local/LocalDataBase.kt
+++ app/src/main/java/com/example/dayflow/data/local/LocalDataBase.kt
@@ -5,8 +5,9 @@ import androidx.room.RoomDatabase
 import com.example.dayflow.data.local.dao.DailyTaskDao
 import com.example.dayflow.data.local.dao.YearlyTaskDao
 import com.example.dayflow.data.local.entity.DailyTaskEntity
+import com.example.dayflow.data.local.entity.YearlyTaskEntity
 
-@Database(entities = [DailyTaskEntity::class], version = 1)
+@Database(entities = [DailyTaskEntity::class, YearlyTaskEntity::class], version = 1)
 abstract class LocalDataBase : RoomDatabase() {
 
     abstract fun dailyTaskDao(): DailyTaskDao
