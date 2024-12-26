4051a1ddfda984e8fa8c874a5237b4ce2459a2df
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-12-26 17:35:12 +0200

    update DefaultNotificationManager be more reusable

--- app/src/main/java/com/example/dayflow/data/utils/NotificationArgs.kt
+++ app/src/main/java/com/example/dayflow/data/utils/NotificationArgs.kt
@@ -2,12 +2,16 @@ package com.example.dayflow.data.utils
 
 import android.app.PendingIntent
 import android.net.Uri
+import androidx.core.app.NotificationCompat
 
 data class NotificationArgs(
     val id: Int,
     val title: String,
+    val text: String? = null,
     val actionText: String = "Stop",
     val actionPendingIntent: PendingIntent? = null,
     val category: String? = null,
-    val ringtone: Uri? = null
+    val ringtone: Uri? = null,
+    val isSilent: Boolean = false,
+    val priority: Int = NotificationCompat.PRIORITY_HIGH
 )
