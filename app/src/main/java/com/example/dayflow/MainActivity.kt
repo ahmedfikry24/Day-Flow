5728b01129672c785447565ed9e4bc77d08cc754
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2025-01-07 11:42:58 +0200

    toggle stats  bar color

--- app/src/main/java/com/example/dayflow/MainActivity.kt
+++ app/src/main/java/com/example/dayflow/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.dayflow
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
@@ -34,7 +36,13 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.auto(
+                Color.TRANSPARENT,
+                Color.TRANSPARENT,
+                detectDarkMode ={ viewModel.state.value.isLightTheme }
+            )
+        )
         installSplashScreen().setKeepOnScreenCondition { !viewModel.initialDataScope.isActive }
         setContent {
             val state by viewModel.state.collectAsStateWithLifecycle()
