70e4c69fb1f3d97cfd74d79ab85c8ef13bb58283
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2025-01-13 10:01:30 +0200

    add BlockNotificationService

---
+++ app/src/main/java/com/example/dayflow/service/BlockNotificationService.kt
@@ -0,0 +1,42 @@
+package com.example.dayflow.service
+
+import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
+import com.example.dayflow.data.local.entity.BlockAppInfoEntity
+import com.example.dayflow.data.usecase.GetAllInstalledAppsUseCase
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+var isBlockNotificationListenerActive = true
+
+@AndroidEntryPoint
+class BlockNotificationService : NotificationListenerService() {
+
+    @Inject
+    lateinit var getAllInstalledAppsUseCase: GetAllInstalledAppsUseCase
+    private val coroutineScope = CoroutineScope(Dispatchers.Default)
+    private var blockedApps = listOf<BlockAppInfoEntity>()
+
+    override fun onCreate() {
+        super.onCreate()
+        coroutineScope.launch {
+            blockedApps = getAllInstalledAppsUseCase()
+        }
+    }
+
+    override fun onNotificationPosted(sbn: StatusBarNotification?) {
+        super.onNotificationPosted(sbn)
+        if (isBlockNotificationListenerActive) {
+            sbn?.let { it.packageName.cancelAppNotification(it.key) }
+        }
+    }
+
+    private fun String.cancelAppNotification(key: String) {
+        if (blockedApps.any { this == it.packageName }) {
+            cancelNotification(key)
+        }
+    }
+}
