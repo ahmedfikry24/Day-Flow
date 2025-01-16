f2279d32c0368f09bc230887e164ad8db59f9124
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2025-01-16 08:16:08 +0200

    test validate require fields

--- app/src/test/java/com/example/dayflow/HelperFunctionTest.kt
+++ app/src/test/java/com/example/dayflow/HelperFunctionTest.kt
@@ -1,6 +1,21 @@
 package com.example.dayflow
 
+import com.example.dayflow.ui.utils.validateRequireField
+import org.junit.Assert
+import org.junit.Test
+
 class HelperFunctionTest {
 
+    @Test
+    fun `given empty string when validate require field then return false`() {
+        val field = ""
+        Assert.assertFalse(field.validateRequireField())
+    }
+
+    @Test
+    fun `given valid string when validate require field then return false`() {
+        val field = "ahmed"
+        Assert.assertTrue(field.validateRequireField())
+    }
 
 }
