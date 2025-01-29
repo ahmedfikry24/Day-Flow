7845d9d7200c935821c5fc8328de7dbdf01d0e07
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2025-01-29 07:48:24 +0200

    test WorkSession update session info visibility

--- app/src/test/java/com/example/dayflow/work_session/WorkSessionViewModelTest.kt
+++ app/src/test/java/com/example/dayflow/work_session/WorkSessionViewModelTest.kt
@@ -1,9 +1,15 @@
 package com.example.dayflow.work_session
 
+import app.cash.turbine.test
 import com.example.dayflow.service.DefaultServiceManager
+import com.example.dayflow.ui.utils.ContentStatus
 import com.example.dayflow.ui.work_session.vm.WorkSessionViewModel
 import com.example.dayflow.utils.BaseViewModelTester
 import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
 
 class WorkSessionViewModelTest : BaseViewModelTester() {
 
@@ -16,5 +22,20 @@ class WorkSessionViewModelTest : BaseViewModelTester() {
         viewModel = WorkSessionViewModel(serviceManager)
     }
 
+    @Test
+    fun `given control session info visibility when start session pressed then update state`() =
+        runTest {
+            viewModel.state.test {
+                val visibleState = awaitItem()
+                assertEquals(ContentStatus.VISIBLE, visibleState.contentStatus)
 
+                viewModel.controlSessionInfoVisibility()
+                val openSessionCountDown = awaitItem()
+                assertTrue(visibleState.isSessionInfoVisible != openSessionCountDown.isSessionInfoVisible)
+
+                viewModel.controlSessionInfoVisibility()
+                assertTrue(openSessionCountDown.isSessionInfoVisible != awaitItem().isSessionInfoVisible)
+                cancelAndIgnoreRemainingEvents()
+            }
+        }
 }
