00a1905bd3eb3d629c61df3dc6bdef10253c2db2
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2025-02-05 14:55:35 +0200

    add espresso-intents dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,6 +86,7 @@ dependencies {
 
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.espresso.intents)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
     androidTestImplementation(libs.androidx.runner)
