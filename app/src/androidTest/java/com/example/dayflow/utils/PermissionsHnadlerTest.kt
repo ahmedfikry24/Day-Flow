8e9740264c48299663abdc9a7b6b6af290732e04
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2025-02-05 17:14:58 +0200

    Add granting schedule alarm test

--- app/src/androidTest/java/com/example/dayflow/utils/PermissionsHnadlerTest.kt
+++ app/src/androidTest/java/com/example/dayflow/utils/PermissionsHnadlerTest.kt
@@ -21,3 +21,15 @@ fun denyNotificationPermission() {
         if (permission.exists()) permission.click()
     }
 }
+
+fun grantScheduleAlarmPermission() {
+    val instrumentation = InstrumentationRegistry.getInstrumentation()
+    val device = UiDevice.getInstance(instrumentation)
+    val app = device.findObject(UiSelector().text("Day Flow"))
+    if (app.exists()) app.click()
+    val permission = device.findObject(UiSelector().text("Allow setting alarms and reminders"))
+    if (permission.exists()) permission.click()
+
+    device.pressBack()
+    device.pressBack()
+}
