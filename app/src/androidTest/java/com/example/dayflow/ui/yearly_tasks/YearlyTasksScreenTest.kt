21722cc573677c406cb5879210bf83ab9e612471
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2025-02-08 13:21:00 +0200

    Create Yearly screen Test

---
+++ app/src/androidTest/java/com/example/dayflow/ui/yearly_tasks/YearlyTasksScreenTest.kt
@@ -0,0 +1,24 @@
+package com.example.dayflow.ui.yearly_tasks
+
+import androidx.activity.compose.setContent
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.navigation.compose.rememberNavController
+import com.example.dayflow.MainActivity
+import com.example.dayflow.utils.BaseAndroidTester
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Rule
+
+@HiltAndroidTest
+class YearlyTasksScreenTest : BaseAndroidTester() {
+
+    @get:Rule(order = 1)
+    val composeRule = createAndroidComposeRule<MainActivity>()
+
+
+    override fun setUp() {
+        super.setUp()
+        composeRule.activity.setContent {
+            YearlyTasksScreen(rememberNavController())
+        }
+    }
+}
