29a3950efd1ae89eb787f141170e998db21e734f
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2025-03-16 14:25:24 +0200

    Add AlarmRescheduleService

---
+++ app/src/main/java/com/example/dayflow/data/alarm/AlarmRescheduleService.kt
@@ -0,0 +1,11 @@
+package com.example.dayflow.data.alarm
+
+import androidx.lifecycle.LifecycleService
+
+class AlarmRescheduleService : LifecycleService() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
