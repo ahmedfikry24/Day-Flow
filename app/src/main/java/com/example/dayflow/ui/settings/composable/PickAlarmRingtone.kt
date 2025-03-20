f18d7bc7922a2f1b59ab4bba800665cf402316f9
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2025-03-20 15:15:10 +0200

    Add media permissions and ringtone selection logic

--- app/src/main/java/com/example/dayflow/ui/settings/composable/PickAlarmRingtone.kt
+++ app/src/main/java/com/example/dayflow/ui/settings/composable/PickAlarmRingtone.kt
@@ -1,9 +1,13 @@
 package com.example.dayflow.ui.settings.composable
 
+import android.Manifest
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.media.RingtoneManager
 import android.net.Uri
+import android.os.Build
 import androidx.activity.compose.ManagedActivityResultLauncher
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.ActivityResult
@@ -36,11 +40,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.dayflow.R
+import com.example.dayflow.ui.composable.PrimaryDialog
 import com.example.dayflow.ui.theme.spacing
+import com.example.dayflow.ui.utils.goToSettings
 import com.example.dayflow.ui.utils.parcelable
 
 @Composable
@@ -65,25 +73,51 @@ fun PickAlarmRingtone(
                 onSaveRingtone(ringtone)
         }
     }
-
+    val context = LocalContext.current
     val musicPicker = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.StartActivityForResult()
     ) { result ->
         val uri = result.data?.data
-        if (uri != null)
+        if (uri != null) {
+            context.contentResolver.takePersistableUriPermission(
+                uri,
+                Intent.FLAG_GRANT_READ_URI_PERMISSION
+            )
             onSaveRingtone(uri)
+        }
+
     }
+    var openMediaRationalDialog by remember { mutableStateOf(false) }
+    var openMediaGoSettingsDialog by remember { mutableStateOf(false) }
+
+    val activity = context as Activity
+    val mediaPermission = if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.S_V2)
+        Manifest.permission.READ_EXTERNAL_STORAGE else Manifest.permission.READ_MEDIA_AUDIO
+    val mediaPermissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission(),
+        onResult = { granted ->
+            when {
+                granted -> launchMediaPicker(musicPicker)
+                ActivityCompat.shouldShowRequestPermissionRationale(
+                    activity,
+                    mediaPermission
+                ) -> openMediaRationalDialog = true
+
+                else -> openMediaGoSettingsDialog = true
+            }
+        }
+    )
+
     Surface(
         modifier = modifier
             .fillMaxWidth()
+            .background(backgroundColor)
             .heightIn(min = 48.dp)
             .padding(horizontal = MaterialTheme.spacing.space8),
         onClick = { isOpen = !isOpen },
     ) {
         Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(backgroundColor),
+            modifier = Modifier.fillMaxWidth(),
             verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.space8)
         ) {
             Row(
@@ -133,7 +167,9 @@ fun PickAlarmRingtone(
                             .height(32.dp)
                             .clickable {
                                 isOpen = false
-                                launchMediaPicker(musicPicker)
+                                if (checkMediaPermission(context, mediaPermission)) {
+                                    launchMediaPicker(musicPicker)
+                                } else mediaPermissionLauncher.launch(mediaPermission)
                             },
                         contentAlignment = Alignment.CenterStart
 
@@ -148,13 +184,27 @@ fun PickAlarmRingtone(
             }
         }
     }
-}
 
-
-@Preview(showSystemUi = true, showBackground = true)
-@Composable
-private fun PreviewPick() {
-    PickAlarmRingtone() {}
+    if (openMediaRationalDialog)
+        PrimaryDialog(
+            title = stringResource(R.string.media_permission),
+            text = stringResource(R.string.to_change_alarm_ringtone_you_must_give_us_media_permission_to_help_you_pick_audio_file),
+            onConfirm = {
+                openMediaRationalDialog = false
+                mediaPermissionLauncher.launch(mediaPermission)
+            },
+            onDismiss = { openMediaRationalDialog = false }
+        )
+    if (openMediaGoSettingsDialog)
+        PrimaryDialog(
+            title = stringResource(R.string.media_permission_needed),
+            text = stringResource(R.string.the_app_needs_media_permission_to_change_your_alarm_ringtone_please_grant_it_in_the_app_settings),
+            onConfirm = {
+                openMediaGoSettingsDialog = false
+                activity.goToSettings()
+            },
+            onDismiss = { openMediaGoSettingsDialog = false }
+        )
 }
 
 private fun launchRingtonePicker(picker: ManagedActivityResultLauncher<Intent, ActivityResult>) {
@@ -175,3 +225,9 @@ private fun launchMediaPicker(launcher: ManagedActivityResultLauncher<Intent, Ac
     launcher.launch(intent)
 }
 
+private fun checkMediaPermission(context: Context, permission: String): Boolean {
+    return ContextCompat.checkSelfPermission(
+        context,
+        permission
+    ) == PackageManager.PERMISSION_GRANTED
+}
